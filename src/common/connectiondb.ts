cb1a83295afd568577506824f23b274517e49bf0
ernestodiazkiki <carlos@kikilogistics.co>
2023-02-15 13:43:14 -0500

    :construction: remove code dead

--- src/common/connectiondb.ts
+++ src/common/connectiondb.ts
@@ -5,16 +5,15 @@ dotenv.config();
 
 const dataSource =  new DataSource({
     type: 'postgres',
-    host: process.env.DB_POSTGRES_HOST || 'localhost',
-    port: parseInt(`${process.env.DB_POSTGRES_PORT}`) || 5432,
-    username: process.env.DB_POSTGRES_USER || 'postgres',
-    password: process.env.DB_POSTGRES_PASSWORD || 'postgres',
-    database: process.env.DB_POSTGRES_DATABASE || 'pop_dev',
+    host: process.env.DB_POSTGRES_HOST,
+    port: parseInt(`${process.env.DB_POSTGRES_PORT}`),
+    username: process.env.DB_POSTGRES_USER,
+    password: process.env.DB_POSTGRES_PASSWORD,
+    database: process.env.DB_POSTGRES_DATABASE,
     entities: [Enterprise]
 })
 
 const getConnect = async () => {
-    console.log(process.env.DB_POSTGRES_USER, ">=================<");
     if(!dataSource.isInitialized) await  dataSource.initialize();
 }
 
