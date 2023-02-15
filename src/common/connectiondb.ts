aa3b7cc16b1aab833755fb7658d3e023fc42b291
ernestodiazkiki <carlos@kikilogistics.co>
2023-02-15 13:42:04 -0500

    :construction: installed dotenv and read environments

--- src/common/connectiondb.ts
+++ src/common/connectiondb.ts
@@ -1,17 +1,20 @@
 import { DataSource } from "typeorm";
 import {Enterprise} from "../entity/Enterprise.entity";
+import dotenv = require("dotenv");
+dotenv.config();
 
 const dataSource =  new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "pop_dev",
+    type: 'postgres',
+    host: process.env.DB_POSTGRES_HOST || 'localhost',
+    port: parseInt(`${process.env.DB_POSTGRES_PORT}`) || 5432,
+    username: process.env.DB_POSTGRES_USER || 'postgres',
+    password: process.env.DB_POSTGRES_PASSWORD || 'postgres',
+    database: process.env.DB_POSTGRES_DATABASE || 'pop_dev',
     entities: [Enterprise]
 })
 
 const getConnect = async () => {
+    console.log(process.env.DB_POSTGRES_USER, ">=================<");
     if(!dataSource.isInitialized) await  dataSource.initialize();
 }
 
